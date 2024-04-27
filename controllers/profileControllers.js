0a27912e741a73093782ad8097937def7170558f
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-27 23:48:45 +0200

    Update profileControllers.js

--- controllers/profileControllers.js
+++ controllers/profileControllers.js
@@ -3,16 +3,27 @@ const prisma = new PrismaClient();
 
 const getProfile = async (req, res) => {
   const userId = req.user.id;
+  console.log(userId);
 
   try {
-    
-    res.status(200).json(
-      {
-        message: "Profile fetched successfully",
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId,
       },
-    );
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        gift: true,
+        lastGift: true,
+      },
+    });
+    console.log(user);
+
+    res.status(200).json(user);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error("Error getting profile:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
