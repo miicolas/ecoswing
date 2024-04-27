379d7f3c999acd1b40bc8b0b6a444ca704230b82
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-27 20:20:13 +0200

    ⚙️

--- ecoswing/api/getProfile.js
+++ ecoswing/api/getProfile.js
@@ -10,20 +10,22 @@ fetch("https://apps-pt-api.kas9uk.easypanel.host/dashboard/getprofile")
     try {
       let profile_container = document.querySelector(".profile_container");
 
-      profile_container.innerHTML += `<div>
-                <h1>Dashboard</h1>
-            </div>
-            <h3>
-                Bienvenue sur votre dashboard EcoSwing, ${data.name} !
-            </h3>
-            <p>Vous avez actuellement ${data.gift} points</p>
-            <p>Prochain tirage dans ${data.lastGift}</p>
-            <button>Tirer votre point</button>
-            <div> Vous pouvez obtenir des réductions à partir de 20 points</div>`;
+      profile_container.innerHTML += `
+        <div class="title-container">
+            <h1 class="title-green">Dashboard</h1>
+        </div>
+        <h3 class="welcome-message">
+            Bienvenue sur votre dashboard EcoSwing, <span class="text-green">${data.name}</span> !
+        </h3>
+        <div class="info-point-container">
+            <p class="points-info">Vous avez actuellement ${data.gift} points</p>
+            <p class="next-draw">Prochain tirage dans ${data.lastGift}</p>
+        </div>
+        <a href="/gift/addgift" ><div class="draw-link">Tirer votre point</div></a>
+        <div class="discount-info">Vous pouvez obtenir des réductions à partir de 20 points</div>
+        <a href="/auth/logout"><div class="logout-link">Se déconnecter</div></a>
+        `;
     } catch (error) {
-      console.error("Error displaying profile:", error);
+      console.log(error);
     }
-  })
-  .catch((error) => {
-    console.error("Error fetching profile:", error);
   });
