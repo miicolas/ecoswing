805b0ce6cf018cab0cda488a4dae81973b185c1c
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-28 10:53:14 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,13 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/authControllers.js";
-import { authenticateToken } from "../middleware/authToken.js";
+
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
-router.get('/data', authenticateToken, (req, res) => {
-    // Route protégée
-    res.json({ message: 'Données sécurisées accessibles !', user: req.user });
-});
+
 
 
 export default router;
